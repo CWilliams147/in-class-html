9256cd6186209c582222e9b26d3850a49b0a60a0
Cameron Williams <cswilliams112004@gmail.com>
2024-02-07 09:11:11 -0500

    feature updates

--- music-app.js/search-results.js
+++ music-app.js/search-results.js
@@ -1,31 +1,54 @@
-'use strict'
+"use strict";
 
 function get(url) {
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'User-Agent': 'CamBam/3.0',
-        },
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "User-Agent": "CamBam/3.0",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data;
-        });
-    }
+    .then(function (data) {
+      return data;
+    });
+}
 
-let topStorage = document.getElementById("topSongsContainer");
+const topStorage = document.getElementById("topSongsContainer");
 const btn = document.getElementById("searchButton");
+const artistReleases = document.getElementById("artistReleases");
 
 let queryString = window.location.search;
 const parameters = new URLSearchParams(queryString);
-const artistId = parameters.get('artistId');
+const artistId = parameters.get("artistId");
 
+get(
+  `https://api.discogs.com/artists/${artistId}?token=bcpQNVIauxqMfkrETtEgDeXDtdMdrXbAtNOMbgWi`
+).then(function (data) {
+  const { name, releases_url } = data;
+  console.log(name);
+  const artistName = document.createElement("h2");
+  artistName.textContent = name;
+  topStorage.append(artistName);
+});
 
-get(`https://api.discogs.com/artists/${artistId}?token=bcpQNVIauxqMfkrETtEgDeXDtdMdrXbAtNOMbgWi`).then(function (
-    data
-) {
-    const { name, releases_url } = data;
-    console.log(name)
+get(
+  `https://api.discogs.com/artists/${artistId}/releases?token=bcpQNVIauxqMfkrETtEgDeXDtdMdrXbAtNOMbgWi`
+).then(function (data) {
+  data.releases.forEach(function (release) {
+    const releaseContainer = document.createElement("div");
+    releaseContainer.classList.add("release-container");
+
+    const releaseTitle = document.createElement("h3");
+    releaseTitle.textContent = release.title;
+
+    const releaseYear = document.createElement("p");
+    releaseYear.textContent = `Year: ${release.year}`;
+
+    releaseContainer.appendChild(releaseTitle);
+    releaseContainer.appendChild(releaseYear);
+
+    artistReleases.appendChild(releaseContainer);
+  });
 });
